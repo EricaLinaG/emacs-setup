dfb7e0e6ae7d5d5c910e07a594cee34b6235d3f7
EricaLinaG <EricaLinaGebhart@gmail.com>
2023-01-17 14:43:55 -0500

    switch to qutebrowser

--- elisp/config/browser_conf.el
+++ elisp/config/browser_conf.el
@@ -3,4 +3,4 @@
 
 ;;(setq browse-url-browser-function 'eww-browse-url)
 (setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "vivaldi-stable")
+      browse-url-generic-program "qutebrowser")
