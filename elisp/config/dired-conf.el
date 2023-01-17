a557efcfa03d9658a955a0a6fc980fd9fa1739c5
EricaLinaG <EricaLinaGebhart@gmail.com>
2023-01-17 14:44:20 -0500

    async mode

--- elisp/config/dired-conf.el
+++ elisp/config/dired-conf.el
@@ -2,6 +2,7 @@
 (diredfl-global-mode)
 ;;(setq diredfl-mode 't)
 (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+(dired-async-mode 1)
 
 ;; I have git gutter already.
 ;; (global-diff-hl-mode)
