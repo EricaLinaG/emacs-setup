06fa9ca73cd5235699f7fa675ba5fa7a5bf866a2
EricaLinaG <EricaLinaGebhart@gmail.com>
2023-01-29 17:41:47 -0500

    Chemacs! Edit readme, put emacsn back.

---
+++ emacs-profiles-orig.el
@@ -0,0 +1,15 @@
+;;; Package --- Summary
+;;; Commentary:
+;;; Code:
+
+(("default" . ((user-emacs-directory . "~/Emacs/stable/elisp")))
+ ("stable"  . ((user-emacs-directory . "~/Emacs/stable/elisp")))
+ ("OG"      . ((user-emacs-directory . "FIXME/elisp")))
+
+ ;;dev ("dev"     . ((user-emacs-directory . "~/Emacs/dev/elisp")))
+ ;;doom ("doom"    . ((user-emacs-directory . "~/Emacs/doom")))
+ ;;space ("space"   . ((user-emacs-directory . "~/Emacs/space")))
+ )
+
+(provide '.emacs-profiles.el)
+;;; emacs-profiles-orig.el ends here
